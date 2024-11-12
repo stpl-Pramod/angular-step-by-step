9571e8db1591e7169abed7db2554b664181350f3
stpl-Pramod <70705666+stpl-Pramod@users.noreply.github.com>
2024-11-12 19:02:05 +0530

    code step by step

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from "./login/login.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular-step-by-step';
+  fetchInput='';
+  count=0;
+  disabled=false;
+  name="peter";
+  show = true;
+  getData(name:string, mob:number){
+    alert(name)
+    alert(mob)
+  }
+  getValue(e:any){
+    console.log("function called",e)
+    // alert(e)
+  }
+  getHover(){
+    console.log("hover called")
+  }
+  getInput(e:any){
+    console.log(e);
+    this.fetchInput = e
+  }
+  counter(type:any){
+type==="minus"?this.count--:this.count++
+  }
+ 
 }
